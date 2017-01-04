7b920a4557f4434bcb8cd6479f6faecf0058648a
liam esteban <leliam.esteban@gmail.com>
2017-01-04 22:28:53 +0100

    Added wifi netctl templates

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -3,18 +3,23 @@
 echo "Dotfiles ... (make them smaller! and add overwrite capabilities)"
 git clone https://github.com/leliamesteban/.dotfiles && cd .dotfiles && ./install
 echo "Installing some software that might be interactive"
-tmux -c packer -S --noconfirm lollypop filezilla lemonbar arbtt gource spotify tomahawk-git arandr youtubedl
-
+tmux -c "packer -S --noconfirm filezilla lemonbar gource spotify arandr youtube-dl numix-circle-icon-theme-git i3lock-fancy-git" 
+echo "Clone $HOME"
 echo "Do the GUI dance ..."
 echo "1. lxappearance
       2. arandr
-      3. sign in to chrome
+      3. sign in to chrome, spotify
       4. set a background
       5. flash
+      6. password managers (enpass + supergenpass
      "
-
+#nasa worldwind
+#google earth
+#liveroot
 #human-git
-#rofi
+#tomahawk-git
+#lollypop
+#arbtt
 #wireshark-gtk
 #gource
 #nix
@@ -30,7 +35,6 @@ echo "1. lxappearance
 #ibam
 #tpacpi-bat
 #batify
-#
 #debian
 #apt-fast
 #blender
@@ -41,3 +45,5 @@ echo "1. lxappearance
 #bootstrap-vz
 #sshfs
 #blender
+#inxi
+#arch wiki cli
